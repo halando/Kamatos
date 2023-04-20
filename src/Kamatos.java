da7d10285c1f91c1082a9be8068c1dd04b881299
Ando Attila <andoattila698@gmail.com>
2023-04-20 13:27:42 +0200

    1437

--- src/Kamatos.java
+++ src/Kamatos.java
@@ -15,8 +15,21 @@ public class Kamatos {
     }
     private double bekerDatas() {
         String betetStr = beker("Betét:");
+      
+        if(!this.checkInput(betetStr)){
+            System.err.println("Hiba! Csak számot lehet beírni.");
+            throw new NumberFormatException("Csak szám");
+        }
         String nevlegesStr = beker("Névleges kamat (%):");
+        if(!this.checkInput(nevlegesStr)){
+            System.err.println("Hiba! Csak számot lehet beírni.");
+            throw new NumberFormatException("Csak szám");
+        }
         String tokesites_szamStr = beker("Évenkénti tőkésítések száma:");
+        if(!this.checkInput(tokesites_szamStr)){
+            System.err.println("Hiba! Csak számot lehet beírni.");
+            throw new NumberFormatException("Csak szám");
+        }
 
         double betet = Double.parseDouble(betetStr);
         double nevleges = Double.parseDouble(nevlegesStr);
--- test/TestKamatos.java
+++ test/TestKamatos.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestKamatos {
@@ -11,7 +11,7 @@ public class TestKamatos {
     
     
 
-    @Before
+    @BeforeEach
    public void init() {
        kamatos = new Kamatos();
     }
